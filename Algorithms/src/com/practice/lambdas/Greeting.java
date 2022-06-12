f755a9c7c431f56952b230eedfb3e25adaa6d438
Harsh Dubey <harsh.dubey@smartbear.com>
2022-06-12 22:37:56 +0530

    Java8 Functional interface annotation.

--- Algorithms/src/com/practice/lambdas/Greeting.java
+++ Algorithms/src/com/practice/lambdas/Greeting.java
@@ -1,6 +1,11 @@
 package com.practice.lambdas;
 
+@FunctionalInterface
 public interface Greeting {
-
     void greet();
+
+    default void anotherGreet() {
+        System.out.println("Default method.");
+    }
+    // void anotherGreet1(); -- Not allowed with Functional interface annotation.
 }
